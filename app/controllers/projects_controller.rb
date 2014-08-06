e28220f51a0f094797516cc7db910ef2cdbe7ffe
Anna Filippova <annafil@gmail.com>
2014-08-06 11:15:39 +0800

    Add projects resource, controller and view

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,6 @@
+class ProjectsController < ApplicationController
+	def new
+	end
+	def create
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+
+  resources :projects  
   resources :codes
 
   get 'welcome/index'
