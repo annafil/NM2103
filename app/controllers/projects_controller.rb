f940256188c16a11af7e31b09012121c95eb1d2a
Anna Filippova <annafil@gmail.com>
2014-08-07 18:01:21 +0800

    Add form validation for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,18 +1,25 @@
 class ProjectsController < ApplicationController
 	def new
 	end
+
 	def create
 		@project = Project.new(project_params)
 	 
-		@project.save
-		redirect_to @project
+		if @project.save
+			redirect_to @project
+		else 
+		    render 'new'
+		end
 	end
+
 	def show
 	  @project = Project.find(params[:id])
 	end
+
 	def index
 	  @projects = Project.all
 	end
+
 	private
 	  def project_params
 	    params.require(:project).permit(:project, :number, :name, :email, :description, :points)
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+	validates :number, :project, :points, :name, :email, presence: true
+	validates_format_of :email, :with => /@/
 end
